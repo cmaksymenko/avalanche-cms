33a93e0eff226eec1a7d9ebad4e3d2f98711e254
Christian Maksymenko <christian.maksymenko@gmail.com>
2024-03-31 21:51:23 +0200

    Implemented logout through Keycloak

--- cli/av/commands/logout.py
+++ cli/av/commands/logout.py
@@ -1,20 +1,30 @@
 import click
+import requests
 import json
 from ..core.token import Token, TokenType, require_token
 
 @click.command()
 @require_token
 def logout():    
+    
+    token = Token.from_type(TokenType.REFRESH)
+
+    keycloak_base_url = 'http://localhost:8080'
+    realm_name = 'avalanchecms'
+
+    logout_url = f"{keycloak_base_url}/realms/{realm_name}/protocol/openid-connect/logout"
+    payload = {'client_id': 'avalanchecli', 'refresh_token': token.encoded_jwt}
+    
     try:
         
-        token = Token.from_type(TokenType.REFRESH)
-        
-        # TODO: logout user
+        response = requests.post(logout_url, data=payload)
+        response.raise_for_status()
         
         cli_response = {"message": "User logged out.", "user_id": token.decoded_jwt.get("sub")}
         
-    except ValueError as e:
-        cli_response = {"error": {"message": str(e)}}
+    except requests.RequestException as e:
+        error_message = f"Logout failed, {e.response.status_code} - {e.response.text}, cache cleared."
+        cli_response = {"error": {"message": error_message}}
     except Exception:
         cli_response = {"error": {"message": "Log out failed, cache cleared."}}
     finally:
